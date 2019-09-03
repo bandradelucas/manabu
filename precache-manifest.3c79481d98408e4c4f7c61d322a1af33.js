fa4221791686a55ce4ae6a3d98ee683c6da0083d
Lucas de Andrade <bandradelucas@gmail.com>
2019-09-02 21:23:10 -0300

    Updates

---
+++ precache-manifest.3c79481d98408e4c4f7c61d322a1af33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f627e0d1f81c76f25db9f28749c435cd",
+    "url": "/manabu/index.html"
+  },
+  {
+    "revision": "ffb6e9c8984fc71fafea",
+    "url": "/manabu/static/css/main.2c065afb.chunk.css"
+  },
+  {
+    "revision": "f7e71d2cca931e5dd749",
+    "url": "/manabu/static/js/2.89e16e46.chunk.js"
+  },
+  {
+    "revision": "ffb6e9c8984fc71fafea",
+    "url": "/manabu/static/js/main.cce703a5.chunk.js"
+  },
+  {
+    "revision": "bc029a33b0b986f1e0c2",
+    "url": "/manabu/static/js/runtime~main.292a5f90.js"
+  }
+]);
