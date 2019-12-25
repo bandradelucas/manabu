28b540598cba7cc8b06364a77bc88820d6c0ebb7
Lucas de Andrade <bandradelucas@gmail.com>
2019-12-25 20:21:06 -0300

    Homepage quebrada em componentes menores, estilização dos componentes

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+
 function App() {
-    return <div>Hello World</div>;
-}
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
+} 
 
 export default App;
