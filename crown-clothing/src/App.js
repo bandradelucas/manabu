94e80b448c0144f7ab56afcd9b4cb247d959e64e
Lucas de Andrade <bandradelucas@gmail.com>
2020-05-17 19:05:33 -0300

    Implemented firebase utils, included ability to store authenticated users into our firestore database

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -7,24 +7,36 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            currentUser: null
+            currentUser: null,
         };
     }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-            console.log(user);
+                userRef.onSnapshot((snapShot) => {
+                    this.setState(
+                        {
+                            id: snapShot.id,
+                            ...snapShot.data(),
+                        },
+                        () => console.log(this.state)
+                    );
+                });
+            }
+
+            this.setState({ currentUser: userAuth });
         });
     }
 
--- crown-clothing/src/firebase/firebase.utils.js
+++ crown-clothing/src/firebase/firebase.utils.js
@@ -10,7 +10,34 @@ const config = {
     storageBucket: 'testing-db-4e294.appspot.com',
     messagingSenderId: '386527327002',
     appId: '1:386527327002:web:c4eb44f5fde6faa850f2a3',
-    measurementId: 'G-MKYKC0SC0T'
+    measurementId: 'G-MKYKC0SC0T',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    console.log(snapShot);
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
 };
 
 firebase.initializeApp(config);
@@ -20,7 +47,7 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
-    prompt: 'select_account'
+    prompt: 'select_account',
 });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
