ac7ec5f1047c42ad2a0342e117201ad8661e1c1e
Lucas de Andrade <bandradelucas@gmail.com>
2021-07-17 11:53:38 -0300

    :construction: Aula 3 ok

---
+++ rocketseat/nlw-together-06/letmeask/src/components/RoomCode.tsx
@@ -0,0 +1,22 @@
+import copyImg from "../assets/images/copy.svg";
+
+import "../styles/room-code.scss";
+
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #-MeI2dcGOmMu1tu1Ev3q</span>
+    </button>
+  );
+}
