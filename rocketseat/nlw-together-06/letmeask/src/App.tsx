bc135ea65524d63d5950736a94ba5f677f3a6b5f
Lucas de Andrade <bandradelucas@gmail.com>
2021-08-03 02:22:42 -0300

    :construction: Aula 4 iniciada

--- rocketseat/nlw-together-06/letmeask/src/App.tsx
+++ rocketseat/nlw-together-06/letmeask/src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
-import { Room } from "./pages/Room";
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
-import { AuthContextProvider } from "./contexts/AuthContext";
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ rocketseat/nlw-together-06/letmeask/src/hooks/useRoom.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { database } from '../services/firebase';
+
+type QuestionType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isHighlighted: boolean;
+  isAnswered: boolean;
+};
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isHighlighted: boolean;
+    isAnswered: boolean;
+  }
+>;
+
+export function useRoom(roomId: string) {
+  const [title, setTitle] = useState('');
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  return { title, questions };
+}
