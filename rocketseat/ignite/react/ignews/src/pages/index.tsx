f6351aff358acd2f53f8b8b69d7fdc32d6871687
Lucas de Andrade <bandradelucas@gmail.com>
2022-10-10 21:31:49 -0300

    :sparkles: Seção Stripe finalizada

--- rocketseat/ignite/react/ignews/src/pages/index.tsx
+++ rocketseat/ignite/react/ignews/src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+
 import { SubscribeButton } from '../components/SubscribeButton/index';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  console.log
   return (
     <>
       <Head>
@@ -15,13 +26,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications<br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LimHpJd3SBrhskd0ClhOu4a');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
