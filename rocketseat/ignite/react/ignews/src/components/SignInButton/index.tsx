ea8360215be81765e2671f0b1907d4c21e76a356
Lucas de Andrade <bandradelucas@gmail.com>
2022-10-17 22:01:53 -0300

    :sparkles: Autenticação com Next Auth ok

--- rocketseat/ignite/react/ignews/src/components/SignInButton/index.tsx
+++ rocketseat/ignite/react/ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
-import { FiX} from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from "next-auth/react"
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Rukado
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn()}
+    >
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
--- rocketseat/ignite/react/ignews/src/pages/_app.tsx
+++ rocketseat/ignite/react/ignews/src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header/index';
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
