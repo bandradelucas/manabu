14d3a8b5e7061a3670603ae7a7c943131a8eff99
Lucas de Andrade <bandradelucas@gmail.com>
2022-11-16 22:17:33 -0300

    :sparkles: Salvando o usuário no banco ok

--- rocketseat/ignite/react/ignews/src/pages/api/auth/[...nextauth].ts
+++ rocketseat/ignite/react/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
+import { query } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,5 +14,28 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  jwt: {
+    signinKey: process.env.SIGNIN_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { user: { email } } = user;
+      
+      try {
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            {
+              data: { email }
+            }
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 }
 export default NextAuth(authOptions)
