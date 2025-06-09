c24336193878f5697f76064d99120a7da2a4eced
Lucas de Andrade <bandradelucas@gmail.com>
2025-06-09 02:07:37 -0300

    feat: adicionado header e footer para testes

--- projects/rukado-web/src/app/(site)/page.tsx
+++ projects/rukado-web/src/app/(site)/page.tsx
@@ -1,5 +1,14 @@
+import { FooterLinks } from "@/components/footer/FooterLinks";
+import { HeaderMegaMenu } from "@/components/header/HeaderMegaMenu";
+import { Button } from "@mantine/core";
+
 export default function Home() {
   return (
-    <h1>yaxxy</h1>
+    <>
+      <HeaderMegaMenu />
+      <h1>Título Teste para Demonstração</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro veniam blanditiis hic omnis quibusdam! Pariatur qui, odio officiis, at fuga sed beatae quas tenetur culpa eius commodi nam facere et?</p>
+      <FooterLinks />
+    </>
   );
 }
---
+++ projects/rukado-web/src/components/footer/FooterLinks.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
+import { ActionIcon, Container, Group, Text } from '@mantine/core';
+import classes from './FooterLinks.module.css';
+
+const data = [
+  {
+    title: 'About',
+    links: [
+      { label: 'Features', link: '#' },
+      { label: 'Pricing', link: '#' },
+      { label: 'Support', link: '#' },
+      { label: 'Forums', link: '#' },
+    ],
+  },
+  {
+    title: 'Project',
+    links: [
+      { label: 'Contribute', link: '#' },
+      { label: 'Media assets', link: '#' },
+      { label: 'Changelog', link: '#' },
+      { label: 'Releases', link: '#' },
+    ],
+  },
+  {
+    title: 'Community',
+    links: [
+      { label: 'Join Discord', link: '#' },
+      { label: 'Follow on Twitter', link: '#' },
+      { label: 'Email newsletter', link: '#' },
+      { label: 'GitHub discussions', link: '#' },
+    ],
+  },
+];
+
+export function FooterLinks() {
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text<'a'>
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          {/* <MantineLogo size={30} /> */}
+          <div>Rukado</div>
+          <Text size="xs" c="dimmed" className={classes.description}>
+            Build fully functional accessible web applications faster than ever
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © 2020 rukado.com. All rights reserved.
+        </Text>
+
+        <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+}
