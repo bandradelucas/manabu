41d5d18c218f271acd76b113e02a355e9504bf57
Lucas de Andrade <bandradelucas@gmail.com>
2025-07-02 18:19:05 -0300

    feat: adicionado PostHog

--- projects/rukado.com/next.config.ts
+++ projects/rukado.com/next.config.ts
@@ -3,6 +3,24 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 const withNextIntl = createNextIntlPlugin();
---
+++ projects/rukado.com/src/instrumentation-client.ts
@@ -0,0 +1,8 @@
+import posthog from "posthog-js";
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  ui_host: "https://us.posthog.com",
+  capture_exceptions: true,
+  debug: process.env.NODE_ENV === "development",
+});
